41c3ff704795b80fb1e2b062f03df12a4275e970
Darielly Santos <daryh_cristina@hotmail.com>
2021-08-27 21:45:37 -0300

    Habilitou recebimento de body de tipo json e criou inicio do endpoint soma

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 var express = require('express');
+
 var app = express();
+app.use(express.json())
 
 app.get("/",function(req,res){
     console.log (req)
     res.send("Olá Mundo")
 })
 
+app.post("/calculator/sum",function(req,res){
+    console.log(req.body)
+    res.status(201)
+})
 //                                localhost 
 // Aplicação disponível em http://127.0.0.1:4000/
 app.listen(4000,function(){console.log("servidor rodando em http://localhost:4000")});
